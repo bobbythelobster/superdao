a2ccb523e76ee2599af7a6c6820141f342a53738
Sam Holmes <sam.lb.holmes@gmail.com>
2026-02-01 20:51:17 -0800

    Meta brand

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,6 +1,9 @@
 // @refresh reload
 import { createHandler, StartServer } from "@solidjs/start/server";
 
+const title = "SuperDAO";
+const description = "A platform for tokenized ventures, enabling governance and ownership in the evolving landscape of our world.";
+
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
@@ -8,7 +11,31 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          
+          {/* Primary Meta Tags */}
+          <title>{title}</title>
+          <meta name="title" content={title} />
+          <meta name="description" content={description} />
+          
+          {/* Favicon */}
+          <link rel="icon" type="image/png" href="/favicon.png" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://superdao.io/" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content="https://superdao.io/og-image.png" />
+          
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://superdao.io/" />
+          <meta property="twitter:title" content={title} />
+          <meta property="twitter:description" content={description} />
+          <meta property="twitter:image" content="https://superdao.io/og-image.png" />
+          
+          {/* Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet" />
